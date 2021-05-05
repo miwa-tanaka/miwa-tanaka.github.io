3766be16474447998815a2b58ad57852eca7120d
Miwa <miwa.tnk9675@gmail.com>
2021-05-05 14:44:43 +0900

    added topbar

---
+++ layouts/topBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <h2>Miwa Tanaka</h2>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+h2 {
+  color: #fff;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: "Kozuka Mincho Pr6N";
+  left: 45%;
+  position: absolute;
+  margin-top: 15px;
+}
+</style>
