09be60fb4d5f0f99d28f19dc337af498e1aaea80
Miwa <miwa.tnk9675@gmail.com>
2021-05-05 14:22:18 +0900

    created main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        miwa-tanaka.github.io
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+     <div class="column backTopLeft">
+      <img src="../assets/img/miwa-1.gif" class="profileImg">
+    </div>
+    <div class="column backTopRight">
+      <h3>About me</h3>
+      <p class="contain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu rutrum justo. Sed posuere pretium nunc, id pulvinar tortor fringilla eu. Mauris fermentum, leo ornare mollis porttitor, nunc mi malesuada neque, eget pellentesque dui nibh vitae lacus. Praesent feugiat aliquet massa, in congue dui efficitur blandit. Suspendisse varius, elit eu pellentesque vehicula, eros tellus luctus nisl, non vulputate felis nulla in arcu. Sed est felis, aliquet id facilisis sed, congue vel massa. Aenean sodales augue in ex feugiat viverra a quis dolor. Nam fermentum efficitur dignissim. Curabitur est neque, sollicitudin eget porttitor a, facilisis id purus. Praesent varius urna nisl, ac volutpat justo commodo non. Nunc lacinia nisi sed metus dapibus aliquam. Maecenas et luctus odio. Vivamus pretium accumsan laoreet. Praesent et pellentesque lacus, eu sagittis dui. Curabitur fringilla bibendum mauris, aliquet bibendum nisl pellentesque sed. Cras non mattis enim, in placerat metus.</p>
+    </div>
+    <div class="column backBottomLeft">
+      <h3>Contact me</h3>
+      <div class="ul-align-center">
+        <ul>
+          <li>
+            <a href="https://github.com/miwa-tanaka" target="_blank">
+              <img src="../assets/img/github.gif" class="icons">
+              https://github.com/miwa-tanaka
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank">
+              <img src="../assets/img/linkedin.gif" class="icons">
+              https://www.linkedin.com/in/miwa-tanaka/
+            </a>
+          </li>
+        </ul>
       </div>
+    </div>
+    <div class="column backBottomRight">
+      <img src="../assets/img/miwa-2.gif" class="profileImg">
     </div>
   </div>
 </template>
@@ -31,43 +36,5 @@
 export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
 </style>
