b2b0d2f6adc76c324c7568a68c6c96e49f114d81
Miwa <miwa.tnk9675@gmail.com>
2021-08-19 18:34:12 +0900

    updated works and contact me

--- layouts/topBar.vue
+++ layouts/topBar.vue
@@ -9,7 +9,7 @@
 h2 {
   color: #000;
   text-shadow: 0px 4px 4px rgba(255, 255, 255, 0.25);
-  font-family: "Kozuka Mincho Pr6N";
+  font-family: "Kozuka Mincho Pr6N", sans-serif;
   left: 45%;
   position: absolute;
   margin-top: 15px;
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
      <div class="column backTopLeft">
-      <!-- <img src="../assets/img/miwa-1.gif" class="profileImg"> -->
       <div class="about">
         <h1>Hello, I'm Miwa Tanaka.</h1>
         <h2>I'm a front-end engineer in Japan.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu rutrum justo. Sed posuere pretium nunc, id pulvinar tortor fringilla eu. Mauris fermentum, leo ornare mollis porttitor, nunc mi malesuada neque, eget pellentesque dui nibh vitae lacus. Praesent feugiat aliquet massa, in congue dui efficitur blandit. Suspendisse varius, elit eu pellentesque vehicula, eros tellus luctus nisl, non vulputate felis nulla in arcu. Sed est felis,</p>
       </div>
     </div>
     <div class="column backTopRight">
@@ -12,28 +12,30 @@
         <img src="../assets/img/miwa-profile.jpg" class="profile_img">
       </div>
     </div>
-    <!-- <div class="column backBottomLeft">
-      <h3>Contact me</h3>
-      <div class="ul-align-center">
-        <ul>
-          <li>
-            <a href="https://github.com/miwa-tanaka" target="_blank">
+    <div class="column works_bg">
+      <h2 class="titles">Works</h2>
+      <div class="works_sqr">Coming Soon...</div>
+      <div class="works_sqr">Coming Soon...</div>
+      <div class="works_sqr">Coming Soon...</div>
+      <div class="works_sqr">Coming Soon...</div>
+    </div>
+    <div class="column contact_bg">
+      <h2 class="titles">Contact Me</h2>
+      <ul>
+        <li>
+          <a href="https://github.com/miwa-tanaka" target="_blank">
               <img src="../assets/img/github.gif" class="icons">
               https://github.com/miwa-tanaka
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank">
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank">
               <img src="../assets/img/linkedin.gif" class="icons">
               https://www.linkedin.com/in/miwa-tanaka/
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div> -->
-    <!-- <div class="column backBottomRight">
-      <img src="../assets/img/miwa-2.gif" class="profileImg">
-    </div> -->
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
