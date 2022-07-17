77423f5a63ae1507c1504b56a4f8fb46b4edeb91
Miwa <miwa.tnk9675@gmail.com>
2022-07-17 15:11:33 +0900

    refacotring

--- pages/index.vue
+++ pages/index.vue
@@ -29,9 +29,9 @@
         </div>
       </section>
       <div class="custom-shape-divider-bottom-1658011892">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-          </svg>
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+        </svg>
       </div>
       <section class="worksWrapper">
         <h4>Works</h4>
@@ -89,73 +89,12 @@
           </ul>
         </div>
       </section>
-      <!-- <section class="name-wrapper">
-        <h1>Miwa Tanaka</h1>
-      </section> -->
-      <!-- <section class="top-wrapper">
-        <div class="top-msg">
-          <div class="top-msg-container">
-            <h2>Hello, I'm Miwa.</h2>
-            <h3>I'm a frontend engineer in Japan.</h3>
-            <p>
-              I am a self-taught Software Engineer. Currently, I am a frontend engineer at ReNK CHANNEL Co, Ltd. in Japan, soaking up as much knowledge and practice as I can.
-              I have 2 years of experience using JavaScript(Vue and React), HTML, and CSS(Sass).
-              In addition to software development, I am passionate about my creative side.
-              I use Figma, AdobeXD, Photoshop, and Illustrator to design and build websites and web applications.
-            </p>
-          </div>
-        </div>
-        <div class="top-img">
-          <div class="top-img-bg">
-            <div class="bg-mask">
-              <img src="../assets/img/miwa-profile.jpg" alt="Miwa Tanaka">
-            </div>
-          </div>
-        </div>
-      </section> -->
-      <!-- <section class="works-wrapper">
-        <h4 class="works-ttl">Works</h4>
-        <div class="works-links">
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/clock-app/">
-              <img src="../assets/img/clock.jpg" alt="clock app">
-            </a>
-            <a href="https://miwa-tanaka.github.io/clock-app/">Clock page</a>
-          </div>
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/fylo-lp/">
-              <img src="../assets/img/fylo.jpg" alt="clock app">
-            </a>
-            <a href="https://miwa-tanaka.github.io/fylo-lp/">Fylo dark theme landing page</a>
-          </div>
-        </div>
-      </section> -->
-      <!-- <section class="contact-wrapper">
-        <h4 class="contact-ttl">Contact Me</h4>
-        <ul class="contact-list">
-          <li>
-            <a href="https://github.com/miwa-tanaka" target="_blank" rel="noopener noreferrer">
-                <img src="../assets/img/github.png" class="icons">
-                https://github.com/miwa-tanaka
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank" rel="noopener noreferrer">
-                <img src="../assets/img/linkedin.png" class="icons">
-                https://www.linkedin.com/in/miwa-tanaka/
-            </a>
-          </li>
-        </ul>
-      </section> -->
     </main>
     <footer>
       <p>&copy; Copyright 2022, Miwa Tanaka</p>
     </footer>
   </div>
 </template>
-
-<script>
-</script>
 
 <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap');
