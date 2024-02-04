d28ff8f58dff9324067510531c2d1625401207d1
Miwa <miwa.tnk9675@gmail.com>
2024-02-04 12:30:04 +0900

    modify: updated styles of contact section

---
+++ components/contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>Get In Touch</h4>
+    <ul class="contactList">
+      <li>
+        <a href="https://github.com/miwa-tanaka" target="_blank" rel="noopener noreferrer">
+          Github
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '~/assets/scss/contact.scss';
+</style>
