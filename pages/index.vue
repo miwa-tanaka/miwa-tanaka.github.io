7e55a79554a64a032daafd4837e96207afb7d231
Miwa <miwa.tnk9675@gmail.com>
2024-02-04 13:09:09 +0900

    refactor: created each components

--- pages/index.vue
+++ pages/index.vue
@@ -1,109 +1,38 @@
 <template>
   <div>
     <header>
-      <nav>
-        <div class="name">
-          <h1>Miwa Tanaka</h1>
-        </div>
-        <span class="position">Software Engineer</span>
-      </nav>
+      <Header />
     </header>
-    <main class="container">
-      <section class="mainVisual">
-        <div class="mainImgWrapper">
-          <div class="mainImg">
-            <h2>Hello, I'm Miwa.</h2>
-            <img src="../assets/img/miwa-profile.jpg" alt="Miwa Tanaka" loading="lazy" width="605" height="807">
-          </div>
-        </div>
-      </section>
-      <section class="aboutWrapper">
-        <div class="aboutContents">
-          <h3>I'm a Software Engineer based in Japan.</h3>
-          <p>
-            I am a self-taught Software Engineer. Currently, I am a frontend engineer at ReNK CHANNEL Co, Ltd. in Japan, soaking up as much knowledge and experience as I can.
-            I have 2 years of experience using JavaScript(Vue and React), HTML, and CSS(Sass).
-            In addition to software development, I am passionate about my creative side.
-            I use Figma, AdobeXD, Photoshop, and Illustrator to design and build websites and web applications.
-          </p>
-        </div>
-      </section>
-      <div class="custom-shape-divider-bottom-1658011892">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-        </svg>
-      </div>
+    <main>
       <section class="worksWrapper">
-        <h4>Works</h4>
-        <div class="worksContents">
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/audiophile/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/img/audiophile.jpg" alt="audiophile" loading="lazy" width="600" height="300">
-            </a>
-            <a href="https://miwa-tanaka.github.io/audiophile/" target="_blank" rel="noopener noreferrer">
-              audiophile eCommerce website using Gatsby
-            </a>
-          </div>
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/the-planets/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/img/theplanets.jpg" alt="clock app" loading="lazy" width="600" height="300">
-            </a>
-            <a href="https://miwa-tanaka.github.io/the-planets/" target="_blank" rel="noopener noreferrer">
-              Planets' fact site using Gatsby and GraphQL
-            </a>
-          </div>
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/clock-app/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/img/clock.jpg" alt="clock app" loading="lazy" width="600" height="300">
-            </a>
-            <a href="https://miwa-tanaka.github.io/clock-app/" target="_blank" rel="noopener noreferrer">
-              Clock using Nuxt.js
-            </a>
-          </div>
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/fylo-lp/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/img/fylo.jpg" alt="fylo" loading="lazy" width="600" height="300">
-            </a>
-            <a href="https://miwa-tanaka.github.io/fylo-lp/" target="_blank" rel="noopener noreferrer">
-              Fylo dark theme landing page using vanilla JavaScript
-            </a>
-          </div>
-          <div class="link">
-            <a href="https://miwa-tanaka.github.io/pomodoro/" target="_blank" rel="noopener noreferrer">
-              <img src="../assets/img/pomodoro.jpg" alt="pomodoro timer" loading="lazy" width="600" height="300">
-            </a>
-            <a href="https://miwa-tanaka.github.io/pomodoro/" target="_blank" rel="noopener noreferrer">
-              Pomodoro timer using React
-            </a>
-          </div>
-        </div>
+        <Works />
       </section>
       <section class="contactWrapper">
-        <h4>Get In Touch</h4>
-        <div class="contactList">
-          <ul>
-            <li>
-              Github:<br>
-              <a href="https://github.com/miwa-tanaka" target="_blank" rel="noopener noreferrer">
-                https://github.com/miwa-tanaka
-              </a>
-            </li>
-            <li>
-              LinkedIn:<br>
-              <a href="https://www.linkedin.com/in/miwa-tanaka/" target="_blank" rel="noopener noreferrer">
-                https://www.linkedin.com/in/miwa-tanaka/
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Contact />
       </section>
     </main>
     <footer>
-      <p>&copy; Copyright 2022, Miwa Tanaka</p>
+      <Footer />
     </footer>
   </div>
 </template>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Castoro&family=Sawarabi+Mincho&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=Poppins:wght@400;500;700&display=swap');
 </style>
+
+<script>
+import Header from '~/components/header'
+import Works from '~/components/works'
+import Contact from '~/components/contact'
+import Footer from '~/components/footer'
+
+export default {
+  components: {
+    Header,
+    Works,
+    Contact,
+    Footer
+  },
+}
+</script>
